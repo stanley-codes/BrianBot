56b2a10001530592f979a74082d6893c1598dfc5
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2020-11-13 14:01:55 -0800

    added intents and hopefully new member works

--- brianbot.py
+++ brianbot.py
@@ -4,11 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
+intents = discord.Intents(members = True, messages = True, guilds = True, presences = True)
+client = discord.Client(intents)
 
-class BrianClient(discord.Client):
-    async def on_ready(self):
-     print(f'{self.user} has connected to Discord!')
+@client.event
+async def on_ready():
+    print(f'{client.user} is ready!')
 
-client = BrianClient() 
+    
+@client.event
+async def on_g_join(member):
+    guild = str(member.guild)
+    name = str(member.displayname)
+    msg = f"Hi {name}! Welcome to {guild}! My name is Brian, I'm a bot here. If you need anything let me know by pinging !help"
+    print(msg)
+    await member.guild.get_channel(706419344070148147).send_message(msg)
+        
+@client.event
+async def on_message(message):
+        channel = str(message.channel)
+        if channel == "testing-bots" and message.author != client.user:
+            await message.channel.send('Hi my name is Brian!')
+
 client.run(BOT_TOKEN)
