acc5e409f893564b01308ff6120e2c44cd887b23
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2020-11-14 15:56:24 -0800

    refactored Brian Bot back to OOP

--- brianbot.py
+++ brianbot.py
@@ -5,25 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents(members = True, messages = True, guilds = True, presences = True)
-client = discord.Client(intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} is ready!')
 
-    
-@client.event
-async def on_g_join(member):
-    guild = str(member.guild)
-    name = str(member.displayname)
-    msg = f"Hi {name}! Welcome to {guild}! My name is Brian, I'm a bot here. If you need anything let me know by pinging !help"
-    print(msg)
-    await member.guild.get_channel(706419344070148147).send_message(msg)
+class BrianBot(discord.Client):
+
+
+    async def on_ready(self):
+        print(f'{self.user} is ready!')
+
+    async def on_member_join(self, member):
+        guild = str(member.guild)
+        name = member.mention
+        msg = f"Hi {name}! Welcome to {guild}! My name is Brian, I'm a bot here. If you need anything let me know by pinging !help"
+        await member.guild.get_channel(706419344070148147).send(msg)
         
-@client.event
-async def on_message(message):
+    async def on_message(self, message):
         channel = str(message.channel)
         if channel == "testing-bots" and message.author != client.user:
             await message.channel.send('Hi my name is Brian!')
-
+client = BrianBot(intents = intents)
 client.run(BOT_TOKEN)
