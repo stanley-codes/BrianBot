c87d431436adcd0fb75ad8731e88371c8de7df01
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2020-11-18 22:11:45 -0800

    add intros to voice channels

--- brianbot.py
+++ brianbot.py
@@ -1,13 +1,18 @@
 import os
 import discord
+import ffmpeg
+from gtts import gTTS
+from ctypes.util import find_library
+from io import BytesIO
 from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv('DISCORD_TOKEN')
-intents = discord.Intents(members = True, messages = True, guilds = True, presences = True)
+intents = discord.Intents(members = True, messages = True, guilds = True, presences = True, voice_states = True)
 
 
 class BrianBot(discord.Client):
+    discord.opus.load_opus(find_library('opus'))
 
 
     async def on_ready(self):
@@ -21,7 +26,43 @@ class BrianBot(discord.Client):
         
     async def on_message(self, message):
         channel = str(message.channel)
-        if channel == "testing-bots" and message.author != client.user:
+        if message.content == '!help':
+            await message.channel.send('!help is currently not available')
+        if channel == "testing-bots" and message.author != self.user:
             await message.channel.send('Hi my name is Brian!')
+    
+    def create_audio_files(self, members):
+        audio_files = []
+        count = 0
+        for member in members:
+            if member.bot:
+                continue
+            audio_file = BytesIO()
+            guild = str(member.guild)
+            name = member.display_name
+            msg = f'Hi {name}! Welcome to {guild}! My name\'s Brian. I am a bot here. If you need anything let me know.'
+            tts = gTTS(msg, lang = 'en')
+            tts.save(f'voice{count}.mp3')
+            pcm = discord.FFmpegPCMAudio(f'voice{count}.mp3')
+            audio_files.append(pcm)
+        return audio_files
+            
+    async def on_voice_state_update(self, member, before, after):
+        voice_channel = after.channel
+        if voice_channel is not None and member != self.user:
+            voice_client = await voice_channel.connect()
+            audio_files = self.create_audio_files(voice_channel.members)
+            if not discord.opus.is_loaded():
+                print('Opus not loaded!')
+                await voice_client.disconnect()
+                return
+            print('Voice connected!')
+            for file in audio_files:
+                voice_client.play(file)
+            if not voice_client.is_playing():
+                await voice_client.disconnect()
+
+        
+
 client = BrianBot(intents = intents)
 client.run(BOT_TOKEN)
